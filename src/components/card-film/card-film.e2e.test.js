c8718ee44eb2b61b3e649fd0a718c103be4e92b0
left38331 <left38331@mail.ru>
2019-11-03 18:54:59 +0300

    add setup-test.js

--- src/components/card-film/card-film.e2e.test.js
+++ src/components/card-film/card-film.e2e.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 import {CardFilm} from "./card-film";
-
-Enzyme.configure({adapter: new Adapter()});
 
 it(`Test click on title`, () => {
   const clickHandler = jest.fn();
